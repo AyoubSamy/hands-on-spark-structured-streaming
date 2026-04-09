b3d928ba0aa34ed55ba0eb0f919962dd30859539
7ayoub15 <SAMY82696@GMAIL.COM>
2026-04-09 11:11:43 +0100

    connecting spark application to load files in hdfs file system

--- src/main/java/net/ayoub/Main.java
+++ src/main/java/net/ayoub/Main.java
@@ -1,17 +1,53 @@
 package net.ayoub;
 
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.streaming.StreamingQuery;
+import org.apache.spark.sql.streaming.StreamingQueryException;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.Metadata;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+import java.util.concurrent.TimeoutException;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws TimeoutException, StreamingQueryException {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        SparkSession sparkSession = SparkSession
+                .builder()
+                .appName("TP6-SparkStructredStreaming")
+                .getOrCreate();
+
+        sparkSession.sparkContext().setLogLevel("ERROR");
+
+        // defining the schema for the files to stream in hdfs ( csv files )
+
+        StructType schema = new StructType(new StructField[]{
+                new StructField("id", DataTypes.StringType,false, Metadata.empty()),
+                new StructField("timestamp", DataTypes.TimestampType,false,Metadata.empty()),
+                new StructField("capteur", DataTypes.StringType,false,Metadata.empty()),
+                new StructField("valeur", DataTypes.DoubleType,false,Metadata.empty()),
+                new StructField("unite", DataTypes.StringType,false,Metadata.empty()),
+        });
+
+
+        Dataset<Row> inputDf = sparkSession
+                .readStream()
+                .option("header", "true")
+                .option("sep", ",")
+                .schema(schema)
+                .csv("hdfs://namenode:8020/streaming");
+
+
+        StreamingQuery query = inputDf.writeStream()
+                .outputMode("append")
+                .format("console")
+                .start();
+
+        query.awaitTermination();
     }
 }
